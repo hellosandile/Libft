111df788ac20368b35ea01b518fb53b646917aec
Sandile MKHIZE <samkhize@c5r1s1.wethinkcode.co.za>
2019-05-23 10:40:00 +0200

    ft_putchar function complete  with notes and main to test

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,11 +6,14 @@
 /*   By: samkhize <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 10:13:11 by samkhize          #+#    #+#             */
-/*   Updated: 2019/05/23 10:36:14 by samkhize         ###   ########.fr       */
+/*   Updated: 2019/05/23 10:39:51 by samkhize         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*putchar is a function that writes a single character to the standard output stream, stdout. The character to be printed is fed into the function as an argument, and if the writing is successful, the argument character is returned. IMPORTANT to remember that the write function is in the unistd.h library */
+/*putchar is a function that writes a single character to the standard output stream, 
+  stdout. The character to be printed is fed into the function as an argument, 
+  and if the writing is successful, the argument character is returned. 
+  IMPORTANT to remember that the write function is in the unistd.h library */
 
 #include<unistd.h>
 
@@ -19,7 +22,8 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-/* This main function used to test if the ft_putchar function works correctly and in this case simply prints out the A character */
+/* This main function used to test if the ft_putchar function works correctly 
+   and in this case simply prints out the A character */
 
 int	main(void)
 {
