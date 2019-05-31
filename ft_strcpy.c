1e54c560eff0e67d43d224439bba2b57336de08d
Sandile MKHIZE <samkhize@c5r1s1.wethinkcode.co.za>
2019-05-31 12:17:51 +0200

    Before lunch today

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: samkhize <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 16:19:49 by samkhize          #+#    #+#             */
-/*   Updated: 2019/05/28 16:53:30 by samkhize         ###   ########.fr       */
+/*   Updated: 2019/05/31 10:15:33 by samkhize         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,41 @@
  * Choose to name it dest or dst
  * */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 char	*ft_strcpy(char *dest, const char *src)
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
-		dest[i] != '\0';
 	}
-	return (dest)
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+*/
+
+int main()
+{
+    char str1[10]= "awesome";
+    char str2[10];
+    char str3[10];
+
+   ft_strcpy(str2, str1);
+   ft_strcpy(str3, "well");
+    puts(str2);
+    puts(str3);
+
+    return 0;
 }
