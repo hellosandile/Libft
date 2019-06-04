e164f06171fdd281bac8c3cf7be10762dd79860d
Event WeThinkCode <event@c5r1s1.wethinkcode.co.za>
2019-06-04 11:59:20 +0200

    Morning normed edits event

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,39 +6,16 @@
 /*   By: samkhize <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 14:13:36 by samkhize          #+#    #+#             */
-/*   Updated: 2019/05/27 13:07:45 by samkhize         ###   ########.fr       */
+/*   Updated: 2019/06/04 10:31:29 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* Chechs whether character is a printable
- 
-  The isprint() function tests for any printing character, including space (' ').
-  The value of the argument must be representable as an unsigned char or the value EOF.
-
-  RETURN VALUES: The isprint() function returns zero 
-f the character tests false and returns non-zero if the character tests true.
- */
+#include "libft.h"
 
 int		ft_isprint(int c)
 {
-	if ( c >= 32 && c <= 126)
+	if (c >= 32 && c <= 126)
 		return (1);
-	return (0);
+	else
+		return (0);
 }
-/* This is anoter way to write this funciton in the c language
-
-int		ft_isprint(int c)
-{
-	return (32 >= c && c <=126);
-}
-
-I could also do this function by placing the ' ' and tilde '~'. This would be difficult
-to do simply beacuse you would need to know what the pc recognises as the first printable
-character and the last one.
-
-int		ft_isprint(int c)
-{
-	return (c >= ' ' && c <= '~'
-}
-
-*/
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,19 +6,13 @@
 /*   By: samkhize <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/01 13:13:32 by samkhize          #+#    #+#             */
-/*   Updated: 2019/06/01 13:45:01 by samkhize         ###   ########.fr       */
+/*   Updated: 2019/06/04 11:47:53 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* The memmove() function copies len bytes from string src to string dst. The two
- * strings may overlap; the copy is always done in a non-destructive manner.
- *
- * RETURN VALUES: The memmove() function returns the original value of dst  */
+#include "libft.h"
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-void	ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 {
 	char *tmp;
 
@@ -28,5 +22,5 @@ void	ft_memmove(void *dst, const void *src, size_t len);
 	ft_memcpy(tmp, src, len);
 	ft_memcpy(dst, tmp, len);
 	free(tmp);
-	return (dst); // deallocates memory
+	return (dst);
 }
