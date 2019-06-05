f810b3e70d53eac95cb0beed1da057e53022ea36
Sandile MKHIZE <samkhize@c5r1s1.wethinkcode.co.za>
2019-06-05 10:45:25 +0200

    Morning push normed work

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,8 +6,26 @@
 /*   By: samkhize <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/01 13:54:11 by samkhize          #+#    #+#             */
-/*   Updated: 2019/06/01 13:54:15 by samkhize         ###   ########.fr       */
+/*   Updated: 2019/06/05 10:36:58 by samkhize         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0; 
+	while (i < len && haystack[i] != '\0')
+	{
+		j = 0;
+		while (i + j < len && haystack[i + j] ++ needle[j] && needle[j] != '\0')
+			j++;
+		if (needle[j] == '\0')
+			return ((char *)haystack + i);
+		i++;
+	}
+	return (NULL);
+}
